ba962ae873835e5b30e07cd15eb6b166b43e7f10
Kiprik <kiprik96@gmail.com>
2017-11-26 15:52:56 +0300

    мэх

--- js/calc.js
+++ js/calc.js
@@ -1,1 +1,10 @@
 $(document).ready(function () {
+
+  raschet.onclick= function() {
+  var res = document.getElementById("price-domen") + document.getElementById("price-host");
+  console.log(res);
+  document.getElementById("final_month").value = res;
+
+}
+
+})
